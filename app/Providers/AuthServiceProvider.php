0b773d96291673a0bc7d08c2aa9413b26dd11320
xandroaprilianto <alexandrosandro58@gmail.com>
2023-07-06 20:36:04 +0700

    AddPermissiontoSideBarMenu

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -21,6 +21,13 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->registerPolicies();
+
+        // Implicitly grant "Super Admin" role all permission checks using can()
+        Gate::before(function ($user, $ability) {
+            if ($user->hasRole('super-admin')) {
+                return true;
+            }
+        });
     }
 }
--- resources/views/app-layout/sidebar.blade.php
+++ resources/views/app-layout/sidebar.blade.php
@@ -1,8 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="dashboard" class="brand-link">
-        <img src="{{ asset('AdminLTE/dist') }}/img/TNIAULogo.png" alt="Lambang_TNI_AU"
-            class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="{{ asset('AdminLTE/dist') }}/img/TNIAULogo.png" alt="Lambang_TNI_AU" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">TAKAH MUSEUM</span>
     </a>
 
@@ -11,8 +10,7 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="{{ asset('AdminLTE/dist') }}/img/user2-160x160.jpg" class="img-circle elevation-2"
-                    alt="User Image">
+                <img src="{{ asset('AdminLTE/dist') }}/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
                 <a href="" class="d-block">{{ auth()->user()->name }}</a>
@@ -21,8 +19,7 @@
 
         <!-- Sidebar Menu -->
         <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
-                data-accordion="false">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
                 <li class="nav-item {{ request()->routeIs('dashboard') ? 'menu-open' : '' }}">
@@ -50,6 +47,7 @@
                         </p>
                     </a>
                 </li>
+                @hasrole('super-admin|admin|kamus')
                 <li class="nav-item {{ request()->routeIs('disposisi') ? 'menu-open' : '' }}">
                     <a href="{{ route('disposisi') }}" class="nav-link">
                         <i class="nav-icon far fa-envelope"></i>
@@ -57,7 +55,8 @@
                             Disposisi
                         </p>
                     </a>
-                </li>   
+                </li>
+                @endhasrole
             </ul>
         </nav>
     </div>
