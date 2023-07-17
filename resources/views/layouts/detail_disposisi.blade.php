f4d49da5cc3881ca992ba4d5e2e59f8ceb09a270
Revanza Marsapala <revanzamarsapala11@gmail.com>
2023-07-17 14:27:33 +0700

    fixing eror action lihat detail disposisi

--- resources/views/layouts/detail_disposisi.blade.php
+++ resources/views/layouts/detail_disposisi.blade.php
@@ -18,37 +18,40 @@
             </div>
         </div>
         <div class="card-body">
-            <!-- <code>{{$data}}</code> -->
-                <div class="form-group row">
-                    <label for="" class="col-sm-2 col-form-label font-weight-normal">Asal Surat</label>
-                    <div class="col-sm-10">
-                        <input type="text" name="" class="form-control" value="{{ $data->suratmasuk->dari }}" readonly>
-                    </div>
+            <!-- <code>{{ $data }}</code> -->
+            <div class="form-group row">
+                <label for="" class="col-sm-2 col-form-label font-weight-normal">Asal Surat</label>
+                <div class="col-sm-10">
+                    <input type="text" name="" class="form-control" value="{{ $data->suratmasuk->dari }}"
+                        readonly>
                 </div>
-                <div class="form-group row">
-                    <label for="" class="col-sm-2 col-form-label font-weight-normal">Pesan Disposisi</label>
-                    <div class="col-sm-10">
-                        <input type="text" name="" class="form-control" value="{{$data->pesan_disposisi}}" readonly>
-                    </div>
+            </div>
+            <div class="form-group row">
+                <label for="" class="col-sm-2 col-form-label font-weight-normal">Pesan Disposisi</label>
+                <div class="col-sm-10">
+                    <input type="text" name="" class="form-control" value="{{ $data->pesan_disposisi }}" readonly>
                 </div>
-                <div class="form-group row">
-                    <label for="" class="col-sm-2 col-form-label font-weight-normal">Status</label>
-                    <div class="col-sm-10">
-                        <input type="text" name="" class="form-control" value="{{$data->suratmasuk->status}}" readonly>
-                    </div>
+            </div>
+            <div class="form-group row">
+                <label for="" class="col-sm-2 col-form-label font-weight-normal">Status</label>
+                <div class="col-sm-10">
+                    <input type="text" name="" class="form-control" value="{{ $data->suratmasuk->status }}"
+                        readonly>
                 </div>
-                <div class="form-group row">
-                    <label for="" class="col-sm-2 col-form-label font-weight-normal">Diteruskan Kpd</label>
-                    <div class="col-sm-10">
-                        <input type="text" name="" class="form-control" value="{{$data->diteruskan->divisi}}" readonly>
-                    </div>
+            </div>
+            <div class="form-group row">
+                <label for="" class="col-sm-2 col-form-label font-weight-normal">Diteruskan Kpd</label>
+                <div class="col-sm-10">
+                    <input type="text" name="" class="form-control" value="{{ $data->diteruskan->divisi }}"
+                        readonly>
                 </div>
-                <div class="form-group row">
-                    <label for="" class="col-sm-2 col-form-label font-weight-normal">Keterangan</label>
-                    <div class="col-sm-10">
-                        <textarea type="text" name="" class="form-control text-bold" readonly>{{$data->suratmasuk->keterangan}}</textarea>
-                    </div>
+            </div>
+            <div class="form-group row">
+                <label for="" class="col-sm-2 col-form-label font-weight-normal">Keterangan</label>
+                <div class="col-sm-10">
+                    <textarea type="text" name="" class="form-control text-bold" readonly>{{ $data->suratmasuk->keterangan }}</textarea>
                 </div>
+            </div>
         </div>
     </div>
 @endsection
